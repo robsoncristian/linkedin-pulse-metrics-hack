6ba93a1cb4a36d43fb47af0176e8a0463170f669
robsoncristian <eu@robsoncristian.com>
2016-12-01 12:25:17 -0200

    Inclusão de versões separadas para Microsoft Excel e Google Drive

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Versão para Microsoft Excel
+
 console.clear();
 console.log("title;views;likes;shares");
 $('div.article-details').each(function(){
@@ -7,4 +9,17 @@ $('div.article-details').each(function(){
   var likes  = article.find(".social-gestures-likes a span.count-container").text();
   var shares  = article.find(".social-gestures-comments a span.count-container").text();
   console.log(title+";"+views+";"+likes+";"+shares);
+});
+
+// Versão para a Planilha do Google Drive
+
+console.clear();
+console.log("title,views,likes,shares");
+$('div.article-details').each(function(){
+  var article = $(this);
+  var title  = article.find(".article-title").text();
+  var views  = article.find(".social-view-count a span.count-container").text();
+  var likes  = article.find(".social-gestures-likes a span.count-container").text();
+  var shares  = article.find(".social-gestures-comments a span.count-container").text();
+  console.log(title+","+views+","+likes+","+shares);
 });
