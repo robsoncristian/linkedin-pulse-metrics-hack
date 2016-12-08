3057ce5dcfe9ab68d945d7172e8dd973a00604d8
robsoncristian <eu@robsoncristian.com>
2016-12-08 21:48:12 -0200

    Remoção de ponto de milhar, ajuste de acentuação, entre outras coisas

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 // Versão para Microsoft Excel
 
 console.clear();
-console.log("title;views;likes;shares");
+console.log("titulo;views;likes;shares;data");
 $('div.article-details').each(function(){
   var article = $(this);
   var title  = article.find(".article-title").text();
   var views  = article.find(".social-view-count a span.count-container").text();
   var likes  = article.find(".social-gestures-likes a span.count-container").text();
-  var shares  = article.find(".social-gestures-comments a span.count-container").text();
-  console.log(title+";"+(parseFloat(views) || 0)+";"+(parseFloat(likes) || 0)+";"+(parseFloat(shares) || 0));
+  var shares = article.find(".social-gestures-comments a span.count-container").text();
+  var date   = article.find(".article-date").text();
+  console.log(unescape(title)+";"+(parseFloat(views.replace(".","")) || 0)+";"+(parseFloat(likes.replace(".","")) || 0)+";"+(parseFloat(shares.replace(".","")) || 0)+";"+unescape(date));
 });
 
 // Versão para a Planilha do Google Drive
 
 console.clear();
-console.log("title,views,likes,shares");
+console.log("titulo,views,likes,shares,data");
 $('div.article-details').each(function(){
   var article = $(this);
   var title  = article.find(".article-title").text();
   var views  = article.find(".social-view-count a span.count-container").text();
   var likes  = article.find(".social-gestures-likes a span.count-container").text();
-  var shares  = article.find(".social-gestures-comments a span.count-container").text();
-  console.log(title+","+(parseFloat(views) || 0)+","+(parseFloat(likes) || 0)+","+(parseFloat(shares) || 0));
+  var shares = article.find(".social-gestures-comments a span.count-container").text();
+  var date   = article.find(".article-date").text();
+  console.log(unescape(title)+","+(parseFloat(views.replace(".","")) || 0)+","+(parseFloat(likes.replace(".","")) || 0)+","+(parseFloat(shares.replace(".","")) || 0)+","+unescape(date));
 });
